6520d8de21dc586dccbbaadb7572485d1637bfe3
vassbence <vbencze2@gmail.com>
2020-08-28 12:43:05 +0200

    serve avatars in webp where possible

--- pages/api/user/avatar/[id].js
+++ pages/api/user/avatar/[id].js
@@ -10,14 +10,26 @@ export default async function getUserAvatar(req, res) {
         .json({ error: `Method ${req.method} Not Allowed` });
     }
 
+    if (!req.query.size) {
+      return res
+        .status(400)
+        .json({ error: "size query parameter is required" });
+    }
+
     const size = parseInt(req.query.size);
 
     const data = await getFromS3(req.query.id);
 
-    res.setHeader("Cache-Control", "max-age=31536000, immutable");
-    res.setHeader("Content-Type", data.ContentType);
+    const acceptsWebP = req.headers?.accept?.split(",")?.includes("image/webp");
 
-    const optimizedImage = await sharp(data.Body).resize(size, size).toBuffer();
+    const optimizedImage = await sharp(data.Body)
+      .resize(size, size)
+      .toFormat(acceptsWebP ? "webp" : "png")
+      .toBuffer();
+
+    res.setHeader("Cache-Control", "max-age=31536000, immutable");
+    res.setHeader("Content-Type", acceptsWebP ? "image/webp" : "image/png");
+
     return res.send(optimizedImage);
   } catch (e) {
     return res.status(500).json({ error: e.message });

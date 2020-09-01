bfc87dc31dd2a5ec0cb01a2234c9e9bc3cf97cac
vassbence <vbencze2@gmail.com>
2020-09-01 19:05:31 +0200

    fix settings footer

--- pages/profil/index.js
+++ pages/profil/index.js
@@ -228,6 +228,7 @@ export default function ProfilePage() {
                             disabled={isSubmitting || !(isValid && dirty)}
                             loading={isSubmitting}
                             inverted
+                            medium
                           >
                             Mentés
                           </Button>

e1b891eba131b7000e4b9f7a6591766d8c6ab4c3
vassbence <vbencze2@gmail.com>
2020-09-04 14:07:40 +0200

    cache headers

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
         headers: [
           {
             key: "Cache-Control",
-            value: "public, max-age=31536000, must-revalidate",
+            value: "public, max-age=31536000, s-maxage=31536000",
           },
         ],
       },

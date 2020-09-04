f883c30e519a059af27612668ca09f733c0e0271
vassbence <vbencze2@gmail.com>
2020-09-04 14:14:59 +0200

    correct cache headers

--- pages/api/user/avatar/[id].js
+++ pages/api/user/avatar/[id].js
@@ -25,7 +25,7 @@ export default async function getUserAvatar(req, res) {
       .toFormat("jpeg")
       .toBuffer();
 
-    res.setHeader("Cache-Control", "public, max-age=31536000, s-maxage=31536000");
+    res.setHeader("Cache-Control", "public, max-age=31536000");
     res.setHeader("Content-Type", "image/jpeg");
     return res.send(optimizedImage);
   } catch (e) {

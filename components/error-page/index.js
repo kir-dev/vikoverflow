ee6963ae1673298e7dbefb1c336d166c6f989e3f
vassbence <vbencze2@gmail.com>
2020-11-17 21:35:17 +0100

    update error page

--- components/error-page/index.js
+++ components/error-page/index.js
@@ -1,24 +1,50 @@
 import styles from "./error-page.module.css";
 import Layout from "components/layout";
 import Link from "next/link";
+import { useMemo } from "react";
 
-export default function ErrorPage({ statusCode, text }) {
+function getErrorMessage(statusCode) {
+  switch (statusCode) {
+    case 404:
+      return "A kért oldal nem található";
+
+    default:
+      return "Sajnálatos hiba lépett fel";
+  }
+}
+
+export default function ErrorPage({ statusCode, showGithub }) {
+  const text = useMemo(() => getErrorMessage(statusCode), [statusCode]);
+
   return (
-    <Layout title={`${statusCode} - vikoverflow`}>
+    <Layout>
       <div className={styles.page}>
         <div className={styles.inner}>
           <h1>{statusCode}</h1>
           <h2>{text}</h2>
+
           <div className={styles.links}>
-            <Link href="/">
-              <a>Főoldal</a>
-            </Link>
-            <Link href="/uj">
-              <a>Új kérdés</a>
-            </Link>
-            <Link href="/profil">
-              <a>Profil</a>
-            </Link>
+            {statusCode !== 404 ? (
+              <a
+                href="https://github.com/kir-dev/vikoverflow/issues/new"
+                target="_blank"
+                rel="noopener"
+              >
+                Küldj be egy issuet!
+              </a>
+            ) : (
+              <>
+                <Link href="/">
+                  <a>Főoldal</a>
+                </Link>
+                <Link href="/uj">
+                  <a>Új kérdés</a>
+                </Link>
+                <Link href="/profil">
+                  <a>Profil</a>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
--- pages/404.js
+++
@@ -1,7 +0,0 @@
-import ErrorPage from "components/error-page";
-
-function Custom404Page() {
-  return <ErrorPage statusCode="404" text="A kért oldal nem található" />;
-}
-
-export default Custom404Page;

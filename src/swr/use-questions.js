1765f83fc30e65a63f256a10951a9e4bdecba4ec
Vass Bence <49574140+vassbence@users.noreply.github.com>
2021-01-19 00:29:29 +0100

    cleanup swr hook (#266)

--- src/swr/use-questions.js
+++ src/swr/use-questions.js
@@ -1,5 +1,4 @@
 import { useSWRInfinite } from "swr";
-import { useCallback, useState } from "react";
 
 export default function useQuestions(opts) {
   const swr = useSWRInfinite((i, prev) => {
@@ -9,20 +8,6 @@ export default function useQuestions(opts) {
 
     return `/api/questions?cursor=${prev.nextCursor}`;
   }, opts);
-
-  const [displayedQuestions, setDisplayedQuestions] = useState(swr.data);
-  // TODO ez a same check itt fos, valahogy az elso oldalt az elso oldallal kene osszehasonlitani
-  // vagy nemtudom, ez eleg mely.. nagyon fura egyutt a vegtelen scroll a new cuccokkal
-  // egyszerre tobb pagenyi uj is johet stb stb...
-  // annyi h az addNewQuestions mindenkeppen gorgessen fel
-  // es resetelheti a questions tombot is, swren belul is vhogy.
-
-  // talan most az egy nagyon easy fix h a leges legelso kerdest csekk h ugyan az e, ha ugyan az akkor same as before minden
-  // ha pedig nem ugyan az show popup, a popuppra kattintva pedig ujrarugni az egesz swrt. (vagy ha ez tul csunya akkor valahogy csak bekonkatenalin az elejere bar szerintem az ordas nagy szopas)
-  const hasNewQuestions = displayedQuestions !== swr.data;
-  const addNewQuestions = useCallback(() => {
-    setDisplayedQuestions(swr.data);
-  }, [swr.data]);
 
   const questions = swr.data
     ? [].concat(...swr.data.map((page) => page.questions))
@@ -42,7 +27,5 @@ export default function useQuestions(opts) {
     isEmpty,
     isReachingEnd,
     loadMore,
-    hasNewQuestions,
-    addNewQuestions,
   };
 }
